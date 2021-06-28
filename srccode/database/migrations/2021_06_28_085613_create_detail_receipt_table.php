3c96626b6dbded2909c2915594e685872e2be8c6
Đặng Minh Quân <67689127+quandangminh2107@users.noreply.github.com>
2021-06-28 15:59:42 +0700

    implement detail receipt

---
+++ srccode/database/migrations/2021_06_28_085613_create_detail_receipt_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailReceiptTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_receipt', function (Blueprint $table) {
+            $table->increments('idDetailReceipt');
+            $table->integer('idDrink');
+            $table->integer('idReceipt');
+            $table->foreign('idDrink')->references('idDrink')->on('Menu');
+            $table->foreign('idReceipt')->references('idReceipt')->on('Receipt');
+            $table->integer('Amount');
+            $table->integer('Sugar')->nullable();
+            $table->integer('Ice')->nullable();
+            $table->tinyInteger('Size');
+            $table->text('DrinkNote');
+            $table->decimal('Price', 22)->default(0.00);
+            $table->timestamps();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_receipt');
+    }
+}
