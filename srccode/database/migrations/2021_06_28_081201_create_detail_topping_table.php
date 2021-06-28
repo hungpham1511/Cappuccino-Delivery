66da80badde87eefd4cd3fbba9ffd16805caf35a
Hưng Phạm <kingpropham@gmail.com>
2021-06-28 15:15:43 +0700

    Implement 'Detail_topping table'

---
+++ srccode/database/migrations/2021_06_28_081201_create_detail_topping_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailToppingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_topping', function (Blueprint $table) {
+            $table->increments('idDetailTopping');
+            $table->foreign('idTopping')->references('idTopping')->on('topping');
+            $table->foreign('idDetailReceipt')->references('idDetailReceipt')->on('detail_receipt');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_topping');
+    }
+}
