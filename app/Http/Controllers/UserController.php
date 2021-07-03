f03ba8f635fb45c62ead362a7b03f35f07d98b5a
minhngoc39 <19520780@gm.uit.edu.vn>
2021-07-03 17:33:46 +0700

    Implement edit user information

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,10 +4,66 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Auth;
 
 class UserController extends Controller
 {
-    public function index(){
-        return view('edituser');
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function edit(User $user)
+    {
+        $user = Auth::user();
+        return view('edituser', compact('user'));
+    }
+    
+    public function update(User $user)
+    {
+        if(Auth::user()->email == request('email')) {
+            $this->validate(request(), [
+                'username' => 'required',
+                //'email' => 'required|email|unique:users',
+                'password' => 'required|min:6',
+                'fullName' => 'required',
+                'gender' => 'required',
+                'phone' => 'required',
+                'address' => 'required',
+            ]);
+
+            $user->username = request('username');
+            //$user->email = request('email');
+            $user->password = bcrypt(request('password'));
+            $user->fullName = request('fullName');
+            $user->gender = request('gender');
+            $user->phone = request('phone');
+            $user->address = request('address');
+
+            $user->save();
+
+            return back();
+        } else {
+            $this->validate(request(), [
+                'username' => 'required',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|min:6|confirmed',
+                'fullName' => 'required',
+                'gender' => 'required',
+                'phone' => 'required',
+                'address' => 'required',
+            ]);
+
+            $user->username = request('username');
+            $user->email = request('email');
+            $user->password = bcrypt(request('password'));
+            $user->fullName = request('fullName');
+            $user->gender = request('gender');
+            $user->phone = request('phone');
+            $user->address = request('address');
+
+            $user->save();
+
+        }
     }
 }
--- resources/views/edituser.blade.php
+++ resources/views/edituser.blade.php
@@ -1,58 +1,144 @@
 @extends('layouts.userlog')
 
 @section('edit')
-<h1 style="text-align: center">Chỉnh sửa thông tin người dùng</h1>
-<div class="container">
-    <form style="border: none;" action="" method="POST" >
-        @csrf
+<div class="row">
+    <div class="navback-btn col-auto position-absolute">
+        <a href="{{ route('orderpage') }}" class="btn">
+            <i class="fa fa-arrow-left" aria-hidden="true"></i>
+        </a>
+    </div>
+</div>
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        
+        <div class="card">
+            <div class="card-header">{{ __('Information') }}</div>
 
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Full Name</strong>
-                    <input type="text" name="fullName" class="form-control" placeholder="Nguyễn Văn A">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Username</strong>
-                    <input type="text" name="username" class="form-control" placeholder="NVA123">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Gender</strong>
-                    <input type="number" name="gender" class="form-control" placeholder="1: Male    2: Female    3: Other">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Phone</strong>
-                    <input type="number" name="phone" class="form-control" placeholder="0124356789">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Address</strong>
-                    <input type="text" name="address" class="form-control" placeholder="13 Mai Văn Vĩnh, Tân Quy, Quận 7, Thành phố Hồ Chí Minh">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Email</strong>
-                    <input type="text" name="email" class="form-control" placeholder="abcxyz@gmail.com">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Password</strong>
-                    <input type="text" name="password" class="form-control" placeholder="nva24rhe3">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-success">Submit</button>
+            <div class="card-body">
+                <form method="post" action="{{route('user.update', $user)}}">
+                    @csrf
+                    {{ method_field('patch') }}
+                    <div class="form-group">
+                        <label class="col-md-12 col-form-label text-md-center">{{ __('Account Security') }}</label>
+                        <div class="form-group row">
+                            <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ $user->username }}" required autocomplete="username" autofocus>
+
+                                @error('username')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $user->email }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                        <div class="col-md-6">
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                        <div class="col-md-6">
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                        </div>
+                    </div>
+               
+
+                    <div class="form-group">
+                        <label class="col-md-12 col-form-label text-md-center">{{ __('Personal Infomation') }}</label>
+                        <div class="form-group row">
+                            <label for="fullName" class="col-md-4 col-form-label text-md-right">{{ __('Full Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="fullName" type="text" class="form-control @error('fullName') is-invalid @enderror" name="fullName" value="{{ $user->fullName }}" required autocomplete="fullName" autofocus>
+
+                                @error('fullName')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="gender" class="col-md-4 col-form-label text-md-right">{{ __('Gender') }}</label>
+                            <div class="col-md-6">
+                                <input id="gender" type="integer" class="form-control @error('gender') is-invalid @enderror" name="gender" placeholder="1: Male    2:Female    3:Orthers"value="{{ $user->gender }}" required autocomplete="gender" autofocus>
+
+                                @error('gender')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Phone Number') }}</label>
+                            <div class="col-md-6">
+                                <input id="phone" type="integer" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ $user->phone }}" required autocomplete="phone" autofocus>
+
+                                @error('phone')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ $user->address }}" required autocomplete="address" autofocus>
+
+                                @error('address')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row mb-0">
+                        <div class="col-md-6 offset-md-4">
+                            <button type="submit" class="btn btn-signin" style="margin-bottom:5%">
+                                {{ __('Edit') }}
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
-    </form>
+    </div>
 </div>
+
 @endsection
