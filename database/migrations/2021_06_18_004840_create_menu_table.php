9c27ef638aedf96ff69ef93b43ebdde608e32921
Đặng Minh Quân <67689127+quandangminh2107@users.noreply.github.com>
2021-07-06 17:14:30 +0700

    Fix table menu

--- database/migrations/2021_06_18_004840_create_menu_table.php
+++ database/migrations/2021_06_18_004840_create_menu_table.php
@@ -19,14 +19,9 @@ class CreateMenuTable extends Migration
             $table->string('category', 50);
             $table->string('picture');
             $table->string('description');
-            $table->decimal('price', 10)->default(0);
-<<<<<<< HEAD
-            $table->timestamps();
-            
-=======
+            $table->decimal('price', 10, 0)->default(0);
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
->>>>>>> 7c49aae (Fix phone number)
         });
     }
 
